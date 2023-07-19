39601eba6530f48f16ca056e59e24e8b0dfb975f
Shiwei Zhang <shizh@microsoft.com>
2023-07-19 18:22:14 +0800

    feat: snapshot PR reviews
    Signed-off-by: Shiwei Zhang <shizh@microsoft.com>

--- cmd/gha/snapshot.go
+++ cmd/gha/snapshot.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
@@ -15,7 +16,25 @@ var snapshotCommand = &cli.Command{
 	ArgsUsage: "<org>/<repo>",
 	Usage:     "take a snapshot of a repository",
 	Aliases:   []string{"s"},
-	Action:    runSnapshot,
+	Flags: []cli.Flag{
+		&cli.BoolFlag{
+			Name:     "pr-reviews",
+			Usage:    "include pull request reviews in the snapshot",
+			OnlyOnce: true,
+		},
+		&cli.IntFlag{
+			Name:     "pr-reviews-ago",
+			Usage:    "include pull request reviews since `DAYS` ago",
+			OnlyOnce: true,
+		},
+		&cli.TimestampFlag{
+			Name:     "pr-reviews-since",
+			Usage:    "include pull request reviews since `DATE`",
+			Config:   cli.TimestampConfig{Layout: time.DateOnly},
+			OnlyOnce: true,
+		},
+	},
+	Action: runSnapshot,
 }
 
 func runSnapshot(ctx *cli.Context) error {
@@ -42,6 +61,72 @@ func runSnapshot(ctx *cli.Context) error {
 		return err
 	}
 	fmt.Println("Saved snapshot to", path)
+
+	if ctx.Bool("pr-reviews") {
+		if err := snapshotPullRequestReviews(ctx, org, repo, client, snapshot); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func snapshotPullRequestReviews(ctx *cli.Context, org string, repo string, client *github.Client, snapshot []byte) error {
+	// parse flags
+	var start time.Time
+	if ago := ctx.Int("pr-reviews-ago"); ago > 0 {
+		start = time.Now().UTC().AddDate(0, 0, int(-ago))
+	}
+	if date := ctx.Value("pr-reviews-since").(time.Time); !date.IsZero() {
+		start = date
+	}
+
+	// fetch pull request reviews
+	issues, err := github.ParseIssues(snapshot)
+	if err != nil {
+		return err
+	}
+	reviews := make(map[int]json.RawMessage)
+	for _, issue := range issues {
+		if issue.IsPullRequest() {
+			if !start.IsZero() && issue.CreatedAt.Before(start) {
+				continue
+			}
+			reviews[issue.Number] = nil
+		}
+	}
+	total := len(reviews)
+	fmt.Printf("Fetching reviews of %d pull requests", total)
+	if !start.IsZero() {
+		fmt.Printf(" since %s", start.Format(time.DateOnly))
+	}
+	fmt.Println("...")
+
+	count := 0
+	for number := range reviews {
+		review, err := client.PullRequestReviews(ctx.Context, org, repo, number)
+		if err != nil {
+			return err
+		}
+		reviews[number] = review
+		count++
+		fmt.Printf(".")
+		if count%50 == 0 {
+			fmt.Printf(" %6g%%\n", float64(10000*count/total)/100.0)
+		}
+	}
+	fmt.Println(strings.Repeat(" ", 50-count%50), "100.00%")
+
+	// save reviews
+	path := fmt.Sprintf("%s_%s_%s_reviews.json", org, repo, time.Now().Format("20060102_150405"))
+	reviewsJSON, err := json.Marshal(reviews)
+	if err != nil {
+		return err
+	}
+	if err := os.WriteFile(path, reviewsJSON, 0644); err != nil {
+		return err
+	}
+	fmt.Println("Saved pull request reviews to", path)
 
 	return nil
 }
--- pkg/github/client.go
+++ pkg/github/client.go
@@ -3,6 +3,7 @@ package github
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -37,19 +38,9 @@ func (c *Client) Snapshot(ctx context.Context, org, repo string) ([]byte, int, e
 		if err != nil {
 			return nil, 0, err
 		}
-		pagedIssues, err := func() ([]json.RawMessage, error) {
-			defer resp.Body.Close()
-			if resp.StatusCode != http.StatusOK {
-				return nil, fmt.Errorf("%s: %s", url, resp.Status)
-			}
-			var items []json.RawMessage
-			if err := json.NewDecoder(resp.Body).Decode(&items); err != nil {
-				return nil, err
-			}
-			return items, nil
-		}()
+		pagedIssues, err := c.decodeResponse(resp)
 		if err != nil {
-			return nil, 0, err
+			return nil, 0, fmt.Errorf("%s: %w", url, err)
 		}
 		issues = append(issues, pagedIssues...)
 		if len(pagedIssues) < 100 {
@@ -63,6 +54,31 @@ func (c *Client) Snapshot(ctx context.Context, org, repo string) ([]byte, int, e
 	return snapshot, len(issues), nil
 }
 
+// PullRequestReviews takes a snapshot of all reviews for a pull request.
+func (c *Client) PullRequestReviews(ctx context.Context, org, repo string, number int) ([]byte, error) {
+	var reviews []json.RawMessage
+	for page := 1; ; page++ {
+		url := fmt.Sprintf("https://api.github.com/repos/%s/%s/pulls/%d/reviews?per_page=100&page=%d", org, repo, number, page)
+		req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+		if err != nil {
+			return nil, err
+		}
+		resp, err := c.do(req)
+		if err != nil {
+			return nil, err
+		}
+		pagedReviews, err := c.decodeResponse(resp)
+		if err != nil {
+			return nil, fmt.Errorf("%s: %w", url, err)
+		}
+		reviews = append(reviews, pagedReviews...)
+		if len(pagedReviews) < 100 {
+			break
+		}
+	}
+	return json.Marshal(reviews)
+}
+
 func (c *Client) do(req *http.Request) (*http.Response, error) {
 	req.Header.Set("Accept", "application/vnd.github+json")
 	apiVersion := c.APIVersion
@@ -74,4 +90,19 @@ func (c *Client) do(req *http.Request) (*http.Response, error) {
 		req.Header.Set("Authorization", "Bearer "+c.Token)
 	}
 	return c.Client.Do(req)
+}
+
+func (c *Client) decodeResponse(resp *http.Response) ([]json.RawMessage, error) {
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		if resp.StatusCode == http.StatusForbidden && c.Token == "" {
+			return nil, fmt.Errorf("%s: provide GITHUB_TOKEN may help", resp.Status)
+		}
+		return nil, errors.New(resp.Status)
+	}
+	var items []json.RawMessage
+	if err := json.NewDecoder(resp.Body).Decode(&items); err != nil {
+		return nil, err
+	}
+	return items, nil
 }
